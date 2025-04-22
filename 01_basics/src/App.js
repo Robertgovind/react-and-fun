0ba7f86a5f6d7052e52686593f067148668b9144
Goivnd Kumar Yadav <robertgovind@gmail.com>
2025-04-22 18:58:05 +0545

    basics of react

---
+++ 01_basics/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  const name = "Govind kr Yadav";
+  return (
+    <>
+      <h1>Hello, this is from App component and this is {name}</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basics/src/hello.js
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h1>Hello, this is from hello function</h1>;
+}
+
+export default Hello;
